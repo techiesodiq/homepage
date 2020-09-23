5094b6bfbbdf686822427f535bfcbba3dc049d67
Sodiq <sodiq.akanmu001@gmail.com>
2020-09-23 15:44:50 +0100

    Updates

---
+++ precache-manifest.aa20f2a2d57435993810d6c2ce5d680a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d65cb9cd5ceaf9751b6c305bce9873",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "d27b88bfd1d8c360c103",
+    "url": "/homepage/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91e934370a89811748eb",
+    "url": "/homepage/static/js/2.acc6c476.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/homepage/static/js/2.acc6c476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27b88bfd1d8c360c103",
+    "url": "/homepage/static/js/main.58deb670.chunk.js"
+  },
+  {
+    "revision": "836f55bc99e549bdd710",
+    "url": "/homepage/static/js/runtime-main.3ed86e83.js"
+  },
+  {
+    "revision": "0a89ec8654c907047573c957bba1e92e",
+    "url": "/homepage/static/media/passport.0a89ec86.jpg"
+  },
+  {
+    "revision": "540c97d5acaa319341844ede2fea2a6b",
+    "url": "/homepage/static/media/photo.540c97d5.jpg"
+  },
+  {
+    "revision": "09da40e4c8d1d014aaabf0354171f52d",
+    "url": "/homepage/static/media/photo001.09da40e4.webp"
+  },
+  {
+    "revision": "5901f6ceab581e9dd0a232b13c608c51",
+    "url": "/homepage/static/media/photo3.5901f6ce.jpg"
+  },
+  {
+    "revision": "e9d21541cef007b19796c6833434c2bb",
+    "url": "/homepage/static/media/photo4.e9d21541.jpg"
+  },
+  {
+    "revision": "fe6937a74ded3873da4a295005d62a79",
+    "url": "/homepage/static/media/photo5.fe6937a7.jpg"
+  },
+  {
+    "revision": "d307cd521a19f6d70d3e6b744d9316df",
+    "url": "/homepage/static/media/photo6.d307cd52.jpg"
+  }
+]);
